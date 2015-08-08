974cf6f7734f30ca2c4dcff9dde6d274d7169989
tbigt <restrada@tbigt.com>
2015-08-08 13:26:45 -0600

    App

---
+++ sv/js/conf.js
@@ -0,0 +1,6 @@
+( document ).bind( 'mobileinit', function(){
+  $.mobile.loader.prototype.options.text = "loading";
+  $.mobile.loader.prototype.options.textVisible = false;
+  $.mobile.loader.prototype.options.theme = "a";
+  $.mobile.loader.prototype.options.html = "";
+});
